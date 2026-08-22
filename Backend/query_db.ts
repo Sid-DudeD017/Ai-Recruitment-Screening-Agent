4da5469aaba45a54f1a4295bc4e07d98e204ccdd
Siddharth Bhakta <realsidboy@gmail.com>
2026-08-22 10:24:58 +0530

    removing error files

--- Backend/query_db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from './src/generated/prisma';
-const prisma = new PrismaClient();
-
-async function main() {
-  const interviews = await prisma.interview.findMany();
-  console.log(interviews.map(i => i.meetingLink));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
